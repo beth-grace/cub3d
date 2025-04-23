8bfa67f6fe002fe5dc2e02560b134b8a6887437f
Coby Adlard <cadlard@F2N015.42adel.org.au>
2025-04-23 12:44:52 +0930

    add color to test script

--- test_parsing.sh
+++ test_parsing.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+WHITE='\033[1;37m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+FAIL=0
+PASS=0
 for bad_file in ./maps/bad/*; do
 	if [ ! -f "$bad_file" ]; then
 		continue
 	fi
-	echo -n "testing $bad_file... "
+	echo -en "${WHITE}testing $bad_file... "
 	./cub3d $bad_file 2>/dev/null >&2
 	if [ $? -eq 0 ]; then
-		echo "KO"
+		echo -e "${RED}FAIL${WHITE}"
+		FAIL=$(($FAIL + 1))
 	else
-		echo "OK"
+		echo -e "${GREEN}PASS${WHITE}"
+		PASS=$(($PASS + 1))
 	fi
 done
+
+echo
+echo "FAIL: $FAIL"
+echo "PASS: $PASS"
